2f457b956edab1a384df4fe80116484d9b481ec0
Ali K <karimiafsharali@gmail.com>
2023-04-17 19:51:57 -0500

    New android project

---
+++ app/src/main/java/us/ak_tech/aktmileagetracker/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package us.ak_tech.aktmileagetracker.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
