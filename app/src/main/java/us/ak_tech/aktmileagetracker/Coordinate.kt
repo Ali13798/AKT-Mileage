2982d4a7e9cd3a5e1b1c56c36defdd2e751ebee3
Ali K <karimiafsharali@gmail.com>
2023-05-08 22:02:46 -0500

    trip and coordinates data classes

---
+++ app/src/main/java/us/ak_tech/aktmileagetracker/Coordinate.kt
@@ -0,0 +1,4 @@
+package us.ak_tech.aktmileagetracker
+
+data class Coordinate(val index: Int, val x: Double, val y: Double) {
+}
---
+++ app/src/main/java/us/ak_tech/aktmileagetracker/Trip.kt
@@ -0,0 +1,12 @@
+package us.ak_tech.aktmileagetracker
+
+import java.util.Date
+import java.util.UUID
+
+data class Trip(
+    val id: UUID,
+    val coordinates: MutableList<Coordinate>,
+    val date: Date,
+    val isForBusiness: Boolean
+) {
+}
