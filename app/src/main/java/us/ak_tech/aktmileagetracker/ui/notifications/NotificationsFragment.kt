d76478d05a04f9909249a27d722be9c86939c461
Ali K <karimiafsharali@gmail.com>
2023-05-08 23:45:15 -0500

    test adding map

--- app/src/main/java/us/ak_tech/aktmileagetracker/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/us/ak_tech/aktmileagetracker/ui/notifications/NotificationsFragment.kt
@@ -7,9 +7,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import us.ak_tech.aktmileagetracker.R
 import us.ak_tech.aktmileagetracker.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -32,8 +38,24 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val mapFragment = SupportMapFragment.newInstance()
+        parentFragmentManager
+            .beginTransaction()
+            .add(R.id.mapView2, mapFragment)
+            .commit()
+        mapFragment.getMapAsync(this)
         return root
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
