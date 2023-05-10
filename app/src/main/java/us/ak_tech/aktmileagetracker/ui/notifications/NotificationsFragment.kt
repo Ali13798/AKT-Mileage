d1c6bb846e764a2de3be2e873dbcf2c3459f212b
Ali K <karimiafsharali@gmail.com>
2023-05-10 11:49:58 -0500

    add map, mark user current location, zoom city lvl

--- app/src/main/java/us/ak_tech/aktmileagetracker/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/us/ak_tech/aktmileagetracker/ui/notifications/NotificationsFragment.kt
@@ -1,27 +1,38 @@
 package us.ak_tech.aktmileagetracker.ui.notifications
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import us.ak_tech.aktmileagetracker.R
 import us.ak_tech.aktmileagetracker.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment(), OnMapReadyCallback {
-
+class NotificationsFragment : Fragment(), OnMapReadyCallback, LocationListener {
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private var map: GoogleMap? = null
+    private var isLocationPermissionGranted = false
+
+    private var lastKnownLocation: Location? = null
+    private lateinit var locationManager: LocationManager
+
+    var coords: LatLng? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,16 +60,102 @@ class NotificationsFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Marker")
-        )
+        map = googleMap
+        getLocationPermission()
     }
 
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            isLocationPermissionGranted = true
+            getLocation()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        isLocationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    isLocationPermissionGranted = true
+                    getLocation()
+                }
+            }
+            else -> {
+                Toast.makeText(
+                    activity,
+                    "Permission Denied. Location permission required.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+    }
+
+
+    private fun getLocation() {
+        locationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2
+            )
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5f, this)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        coords = LatLng(location.latitude, location.longitude)
+        binding.textNotifications.text =
+            "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+
+        map?.addMarker(
+            MarkerOptions()
+                .position(coords!!)
+                .title("Marker")
+        )
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(coords!!, 10.0f))
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
     }
 }
