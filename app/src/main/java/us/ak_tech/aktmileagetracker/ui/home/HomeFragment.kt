cd6f229e47eda781da8d8eefd25e38476aa59af2
Ali K <karimiafsharali@gmail.com>
2023-05-15 00:12:37 -0500

    record and add new trips to db

--- app/src/main/java/us/ak_tech/aktmileagetracker/ui/home/HomeFragment.kt
+++ app/src/main/java/us/ak_tech/aktmileagetracker/ui/home/HomeFragment.kt
@@ -1,17 +1,38 @@
 package us.ak_tech.aktmileagetracker.ui.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import us.ak_tech.aktmileagetracker.Coordinate
+import us.ak_tech.aktmileagetracker.R
+import us.ak_tech.aktmileagetracker.Trip
 import us.ak_tech.aktmileagetracker.databinding.FragmentHomeBinding
+import us.ak_tech.aktmileagetracker.ui.details.TAG
+import java.time.LocalDateTime
+import java.util.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), LocationListener {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var locationManager: LocationManager
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,17 +43,103 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding.apply {
+            btnStart.setOnClickListener {
+                toggleRecording()
+            }
+        }
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-        return root
+        return binding.root
+    }
+
+    private fun toggleRecording() {
+        if (homeViewModel.isTripRecording)
+            stopRecording()
+        else
+            startRecording()
+    }
+
+    private fun startRecording() {
+        binding.btnStart.text = getString(R.string.stop_trip)
+        homeViewModel.apply {
+            isTripRecording = true
+            coordinateIndex = 0
+            tripId = UUID.randomUUID()
+            startDate = LocalDateTime.now()
+        }
+        getLocation()
+    }
+
+    private fun stopRecording() {
+        binding.btnStart.text = getString(R.string.start_trip)
+        homeViewModel.apply {
+            isTripRecording = false
+            endDate = LocalDateTime.now()
+
+            if (coordinateIndex == 0) {
+                reset()
+                return
+            }
+            var newTrip = Trip(tripId!!, coordinates, startDate!!, endDate!!, isBusinessTrip)
+            trip = newTrip
+            viewLifecycleOwner.lifecycleScope.launch {
+                homeViewModel.addTrip(newTrip)
+            }
+        }
+        reset()
+    }
+
+    fun reset() {
+        homeViewModel.apply {
+            tripId = null
+            startDate = null
+            endDate = null
+            isBusinessTrip = false
+            coordinateIndex = 0
+            coordinates = mutableListOf<Coordinate>()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        stopRecording()
         _binding = null
     }
+
+    override fun onLocationChanged(location: Location) {
+        if (!homeViewModel.isTripRecording) return
+
+        if (homeViewModel.tripId == null) {
+            homeViewModel.tripId = UUID.randomUUID()
+        }
+
+        var id = homeViewModel.tripId!!
+        val coordinate =
+            Coordinate(id, homeViewModel.coordinateIndex++, location.longitude, location.latitude)
+        Log.d(TAG, coordinate.toString())
+
+        homeViewModel.coordinates += coordinate
+    }
+
+    private fun getLocation() {
+        locationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2
+            )
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 5f, this)
+    }
+
+
 }
--- app/src/main/java/us/ak_tech/aktmileagetracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/us/ak_tech/aktmileagetracker/ui/home/HomeViewModel.kt
@@ -3,6 +3,11 @@ package us.ak_tech.aktmileagetracker.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import us.ak_tech.aktmileagetracker.Coordinate
+import us.ak_tech.aktmileagetracker.Trip
+import us.ak_tech.aktmileagetracker.TripsRepository
+import java.time.LocalDateTime
+import java.util.UUID
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +15,22 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+    val tripsRepository = TripsRepository.get()
+    var isTripRecording = false
+    var coordinateIndex = 0
+    var coordinates = mutableListOf<Coordinate>()
+    var tripId: UUID? = null
+    var trip: Trip? = null
+    var startDate: LocalDateTime? = null
+    var endDate: LocalDateTime? = null
+    var isBusinessTrip = false
+
+
+    suspend fun loadTrips(): List<Trip> {
+        return tripsRepository.getTrips()
+    }
+
+    suspend fun addTrip(trip: Trip) {
+        tripsRepository.addTrip(trip)
+    }
 }
